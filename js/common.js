57320658a55ba943fc5d0dd11bce1af3d2f48b53
Dulkith <dulkith@outlook.com>
2021-01-17 14:03:50 +0530

    add js

---
+++ js/common.js
@@ -0,0 +1,61 @@
+function startJourney() {
+    document.location.href = "./introductoryPage01.html";
+}
+
+
+
+// navigation functions
+function favourite() {
+    document.location.href = "./favourites.html";
+}
+
+function home() {
+    document.location.href = "./home.html";
+}
+
+function messages() {
+    document.location.href = "./messages.html";
+}
+
+function cart() {
+    document.location.href = "./cart.html";
+}
+
+function profile() {
+    document.location.href = "./profile.html";
+}
+
+function product() {
+    document.location.href = "./product.html";
+}
+
+function goToMyOrders() {
+    document.location.href = "./order.html";
+}
+
+function goToFeedback() {
+    document.location.href = "./feedback.html";
+}
+
+function goToAR() {
+    document.location.href = "./arForProduct.html";
+}
+
+
+{/* <div class="post">
+    <img src="../assets/images/favourite/tshirt1-blue.png" alt="" srcset="" class="post-image">
+        <div class="post-preview">
+            <h4>2019 Spring Brand Clothing Men's Long Sleeve Round Neck T-Shirt</h4>
+            <p class="store-detail"><strong>Store</strong> Trendy Fashion Store</p>
+            <h3 class="price-tag">LKR 2199.00</h3>
+            <!-- <i class="fas fa-heart heart-icon"></i> -->
+                        </div>
+                    </div>
+    <div class="post">
+        <img src="../assets/images/favourite/image2.jpeg" alt="" srcset="" class="post-image">
+            <div class="post-preview">
+                <h4>Hayiou gt1 tws water proof Finger print Touch Blutooth Earphones</h4>
+                <p class="store-detail"><strong>Store</strong> 3C Global Tradings</p>
+                <h3 class="price-tag">LKR 1500.00</h3>
+            </div>
+                    </div> */}
---
+++ js/map-location.js
@@ -0,0 +1,40 @@
+function initMap() {
+    //Give lat and lang of the location
+    let place = {lat: 6.864279, lng: 79.902030};
+
+    let map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17,
+        center: place,
+        streetViewControl: true,
+        panControl: true,
+        zoomControl: true,
+        mapTypeControl: true,
+        /*mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+        },*/
+        scaleControl: true,
+        overviewMapControl: true,
+        rotateControl: true,
+        draggable: false //to avoid user getting lost in the map
+    });
+
+    //Code for the marker
+    let marker = new google.maps.Marker({
+        position: place,
+        icon:'../assets/images/StoreMap/restaurant.png',
+        animation: google.maps.Animation.BOUNCE,
+        map: map
+    });
+
+    var infowindow = new google.maps.InfoWindow({
+        content: "This is the location of Trendy Fashion Store!"
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.open(map, marker);
+    });
+}
+
+function goToProduct() {
+    document.location.href = "./product.html";
+}
